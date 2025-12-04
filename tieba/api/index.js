8b657f11c631c74f32ec04f0949aac06f90d0650
YanMo <3351708803@qq.com>
2025-12-04 21:03:26 +0800

    Delete tieba directory

--- tieba/api/index.js
+++
@@ -1,6 +0,0 @@
-const serverless = require('serverless-http');
-const path = require('path');
-// require the app exported from server.js
-const app = require(path.join(__dirname, '..', 'server'));
-
-module.exports = serverless(app);

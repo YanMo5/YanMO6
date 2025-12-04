890dd0aedb48a96e4cb7fa7275d590b4dbd9cccb
YanMo5 <3351708803@qq.com>
2025-12-04 20:38:07 +0800

    F:/www/tieba

---
+++ tieba/README.md
@@ -0,0 +1,21 @@
+# 简易贴吧（Tieba Clone）
+
+这是一个最小可运行的贴吧示例，使用 Node.js + Express + EJS，并用简单的 JSON 文件作为存储。
+
+功能：
+- 查看板块
+- 在板块内查看帖子
+- 发新帖
+- 回复帖子
+
+快速上手：
+
+```powershell
+cd f:\www\tieba
+npm install
+npm start
+```
+
+然后在浏览器打开 http://localhost:3000
+
+数据文件位于 `data/db.json`，你可以直接编辑以初始化更多板块或帖子。
---
+++ tieba/server.js
@@ -0,0 +1,64 @@
+const express = require('express');
+const path = require('path');
+const bodyParser = require('body-parser');
+const dotenv = require('dotenv');
+const db = require('./db');
+
+dotenv.config();
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+app.use(express.static(path.join(__dirname, 'public')));
+app.use(bodyParser.urlencoded({ extended: false }));
+
+app.get('/', (req, res) => {
+  const boards = db.getBoards();
+  res.render('index', { boards });
+});
+
+app.get('/board/:boardId', (req, res) => {
+  const board = db.getBoard(req.params.boardId);
+  if (!board) return res.status(404).send('板块未找到');
+  res.render('board', { board });
+});
+
+app.get('/board/:boardId/new', (req, res) => {
+  const board = db.getBoard(req.params.boardId);
+  if (!board) return res.status(404).send('板块未找到');
+  res.render('newThread', { board });
+});
+
+app.post('/board/:boardId/new', (req, res) => {
+  const boardId = req.params.boardId;
+  const { title, author, content } = req.body;
+  try {
+    const thread = db.createThread(boardId, { title, author, content });
+    res.redirect(`/thread/${thread.id}`);
+  } catch (err) {
+    res.status(400).send(err.message);
+  }
+});
+
+app.get('/thread/:threadId', (req, res) => {
+  const thread = db.getThread(req.params.threadId);
+  if (!thread) return res.status(404).send('帖子未找到');
+  res.render('thread', { thread });
+});
+
+app.post('/thread/:threadId/reply', (req, res) => {
+  const threadId = req.params.threadId;
+  const { author, content } = req.body;
+  try {
+    db.addReply(threadId, { author, content });
+    res.redirect(`/thread/${threadId}`);
+  } catch (err) {
+    res.status(400).send(err.message);
+  }
+});
+
+app.listen(PORT, () => {
+  console.log(`Tieba clone running at http://localhost:${PORT}`);
+});
